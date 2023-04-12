c349570feeb55a9f62ab12443c36ca3b9102b6b6
Abigail Ramirez <abigail.ramirez@pucp.edu.pe>
2023-04-11 20:45:47 -0500

    first commit

---
+++ src/main/java/com/example/lab3_20191822/Lab320191822Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20191822;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320191822Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320191822Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab3_20191822/Lab320191822ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20191822;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320191822ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
